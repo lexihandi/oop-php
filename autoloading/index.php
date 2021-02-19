e380a10a9c90b7472f7b151b6b478733fc5a8729
lexyhandi <lexihandi@gmail.com>
2021-02-19 16:42:54 +0700

    update

---
+++ autoloading/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'App/init.php';
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Call of Duty", "Gareth Evans", "Activision", 120000, 50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+echo $cetakProduk->cetak();
---
+++ namespace/App/Produk/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Produk;
+
+class User
+{
+    public function __construct()
+    {
+        echo "ini adalah " . __CLASS__;
+    }
+}
